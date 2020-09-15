a4cd4b978c09952a1a4c02e2d9882368cd30befc
unknown <injimess@gmail.com>
2020-09-15 23:58:04 +0200

    update Product

--- src/main/java/com/company/controller/ProductController.java
+++ src/main/java/com/company/controller/ProductController.java
@@ -8,8 +8,12 @@ import reactor.core.publisher.Mono;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,15 +30,29 @@ public class ProductController {
 	Logger logger = LoggerFactory.getLogger(ProductController.class); 
 	
 	@PostMapping("/products")
-	Mono<Product> postProducts(@Valid @RequestBody Product product){
+	public Mono<Product> postProducts(@Valid @RequestBody Product product){
 		
 		return productRepo.save(product);
 	}
 	
 	@GetMapping("/products")
-	Flux<Product> getProducts(){
+	public Flux<Product> getProducts(){
 		
 		return productRepo.findAll();
 	}
+	
+	@GetMapping("/products/{id}")
+	public Mono<ResponseEntity<Product>> getProductById(@PathVariable(value = "id") String productId){
+		return productRepo.findById(productId).map(savedProduct -> ResponseEntity.ok(savedProduct))
+				.defaultIfEmpty(ResponseEntity.notFound().build());	
+	}
 
+	@PutMapping("/products/{id}")
+	public Mono<ResponseEntity<Product>> updateProduct(@PathVariable (value = "id") String productId, @Valid @RequestBody Product product){
+		return productRepo.findById(productId).flatMap(existingProduct-> {
+			existingProduct.setProductName(product.getProductName());
+			return productRepo.save(existingProduct);
+		}).log().map(updateProduct -> new ResponseEntity<>(updateProduct, HttpStatus.OK)).log()
+				.defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 }
